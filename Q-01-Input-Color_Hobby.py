63b3be3084c3af8058ff2e3435f10e2abb5e60be
unknown <lochanaeg@gmail.com>
2025-01-06 14:35:55 +0530

    Third Commit

---
+++ Q-01-Input-Color_Hobby.py
@@ -0,0 +1,11 @@
+#6/1/2025
+#This is the python code for user input 'Color' and 'Hobby'
+
+#The input from the user
+ColorInput = input("So what is your favorite color: ")
+HobbyInput = input("What is  your favorite hobby may I ask: ")
+
+
+#Print out the input
+print("Fabulous!! You like",ColorInput,"color and your hobby is",
+      HobbyInput,"which is cool!")
