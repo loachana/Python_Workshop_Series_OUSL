1737b09f61eef5fffe93b65772ea4c01419ad23e
unknown <lochanaeg@gmail.com>
2025-01-06 14:12:52 +0530

    Second Commit

---
+++ Q-01-Input-Age.py
@@ -0,0 +1,8 @@
+#6/1/2025
+#This is the python code for user input 'age'
+
+#The input from the user
+AgeInput = input("I would love to know your age: ")
+
+#Print out the input
+print("That is fantastic! Congratulations for being", AgeInput, "years old!")
