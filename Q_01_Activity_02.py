68bf19def6b5bb04c9b1f2851340a192ca58ae72
unknown <lochanaeg@gmail.com>
2025-01-10 10:33:52 +0530

    Activity 2

---
+++ Q_01_Activity_02.py
@@ -0,0 +1,34 @@
+'''
+9/1/2025
+This is the second activity of the Python Workshop series.
+All rights reserved.
+'''
+
+'''
+part 1
+-------------------------------------------------------------
+'''
+
+#class 'int'
+Age_in_Years = int(30)
+
+#class 'float'
+Height_in_Meters = float(1.82)
+
+#class 'str'
+First_Name = str("Lochana")
+
+
+'''
+part 2
+-------------------------------------------------------------
+'''
+
+#print str
+print("Name:",First_Name)
+
+#print int
+print("Age:", Age_in_Years)
+
+#print float
+print("Height in Meters:", Height_in_Meters)
