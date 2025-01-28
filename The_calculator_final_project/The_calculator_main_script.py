23fa11e9476fd48cf2ed24f6b3bbf8723c1dc3db
unknown <lochanaeg@gmail.com>
2025-01-28 13:27:39 +0530

    Cal_Update

--- The_calculator_final_project/The_calculator_main_script.py
+++ The_calculator_final_project/The_calculator_main_script.py
@@ -24,70 +24,47 @@ r = 1
 user_input1 = ""
 user_input2 = ""
 the_sum = 0
+adder = 0
 
 # Function to handle button click event
 def plus_button():
     global r
     global user_input1
-    global user_input2
     global the_sum
+    global adder
     
-    if r%2 == 1:
-        user_input1 = entry1.get()
-        #print(user_input1)
-        the_sum = the_sum + int(user_input1)
-        if user_input1 == user_input2:
-            the_sum = the_sum + int(user_input2)
-        else:
-            pass
-        #entry1.delete(0,"end")
-        if r == 1:
-            entry1.insert(0, int(user_input1))
-            entry1.delete(0,"end")
-        else:
-            entry1.delete(0,"end")
-            entry1.insert(0, the_sum)
+    user_input1 = entry1.get()
 
-        print(the_sum)
-        r = r+1
-        
-    elif r%2 == 0:
-        user_input2 = entry1.get()
-        
-        #print(user_input1)
-        the_sum = the_sum + int(user_input2)
-
-        if user_input1 == user_input2:
-            the_sum = the_sum + int(user_input1)
-        else:
-            pass
-        
-        
-       
-        entry1.delete(0,"end")
+    if r == 2:
+        adder = user_input1
+        the_sum = the_sum + int(adder)
+        entry1.delete(0, "end")
         entry1.insert(0, the_sum)
-        r = r+1
+    elif r == 1:
+        the_sum = the_sum + int(user_input1)
+        entry1.delete(0, "end")
+    else:
+        the_sum = the_sum + int(adder)
+        entry1.delete(0, "end")
+        entry1.insert(0, the_sum)
 
-        print(the_sum)
+    r = r + 1
 
-    #user_input2 = entry1.get()
-
-    #the_sum = new1 + user_input2
-    #entry1.delete(0,"end")
-    #entry1.insert(0,the_sum)
+    print(the_sum)
+    
 
 
 
 root = tk.Tk()
-root.title("Get User Name")
-root.geometry("300x300")
+root.title("The Simple Calculator")
+root.geometry("600x600")
 
 # Label
-label = tk.Label(root, text="Enter your name:", font=("Arial", 12))
+label = tk.Label(root, text="The Simple Calculator", font=("Arial", 20))
 label.pack(pady=10)
 
 # Entry widget
-entry1 = tk.Entry(root)
+entry1 = tk.Entry(root, width=100, font=('Arial 30'), justify="right")
 entry1.pack(pady=5)
 
 
