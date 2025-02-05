cd076e345f8c891af41153ae8ae7c8c0971a159a
unknown <lochanaeg@gmail.com>
2025-02-05 20:54:01 +0530

    Tic_Update

--- The_Tic_Tac_Toe/Main_Script.py
+++ The_Tic_Tac_Toe/Main_Script.py
@@ -50,20 +50,47 @@ x_sec_diagonal = 0
 def button_1_c():
     global r
     global _1x1
-    global _1x123
-    global _123x1
-    global _pri_diagonal
-
+    global o_1x123
+    global o_123x1
+    global o_pri_diagonal
+    global x_1x123
+    global x_123x1
+    global x_pri_diagonal
+    
     
     if r%2 == 0:
         button_1.config(text="X")
         button_1.config(state=tk.DISABLED)
+
+        x_1x123 = x_1x123 + _1x1
+        x_123x1 = x_123x1 + _1x1
+        x_pri_diagonal = x_pri_diagonal + _1x1
+
+        if x_1x123 == 3:
+            print("x win")
+        elif x_123x1 == 3:
+            print("x win")
+        elif x_pri_diagonal == 3:
+            print("x win")
+        
     elif r%2 == 1:
         button_1.config(text="O")
         button_1.config(state=tk.DISABLED)
+
+        o_1x123 = o_1x123 + _1x1
+        o_123x1 = o_123x1 + _1x1
+        o_pri_diagonal = o_pri_diagonal + _1x1
+
+        if o_1x123 == 3:
+            print("o win")
+        elif o_123x1 == 3:
+            print("o win")
+        elif o_pri_diagonal == 3:
+            print("o win")
+        
     r = r + 1
 
-
+'''
     _1x123 = _1x123 + _1x1
     _123x1 = _123x1 + _1x1
     _pri_diagonal = _pri_diagonal + _1x1
@@ -75,22 +102,43 @@ def button_1_c():
         print("win")
     elif _pri_diagonal == 3:
         print("win")
+'''
 
 
 def button_2_c():
     global r
     global _1x2
-    global _1x123
-    global _123x2
+    global o_1x123
+    global o_123x2
+    global x_1x123
+    global x_123x2
     
     if r%2 == 0:
         button_2.config(text="X")
         button_2.config(state=tk.DISABLED)
+
+        x_1x123 = x_1x123 + _1x2
+        x_123x2 = x_123x2 + _1x2
+
+        if x_1x123 == 3:
+            print("x win")
+        elif x_123x2 == 3:
+            print("x win")
+            
     elif r%2 == 1:
         button_2.config(text="O")
         button_2.config(state=tk.DISABLED)
-    r = r + 1
 
+        o_1x123 = o_1x123 + _1x2
+        o_123x2 = o_123x2 + _1x2
+
+        if o_1x123 == 3:
+            print("o win")
+        elif o_123x2 == 3:
+            print("o win")
+            
+    r = r + 1
+'''
     _1x123 = _1x123 + _1x2
     _123x2 = _123x2 + _1x2
     
@@ -98,25 +146,53 @@ def button_2_c():
         print("win")
     elif _123x2 == 3:
         print("win")
-
+'''
 
 
 def button_3_c():
     global r
     global _1x3
-    global _1x123
-    global _123x3
-    global _sec_diagonal
-
+    global o_1x123
+    global o_123x3
+    global o_sec_diagonal
+    global x_1x123
+    global x_123x3
+    global x_sec_diagonal
     
     if r%2 == 0:
         button_3.config(text="X")
         button_3.config(state=tk.DISABLED)
+
+        x_1x123 = x_1x123 + _1x3
+        x_123x3 = x_123x3 + _1x3
+        x_sec_diagonal = x_sec_diagonal + _1x3
+
+        if x_1x123 == 3:
+            print("x win")
+        elif x_123x3 == 3:
+            print("x win")
+        elif x_sec_diagonal == 3:
+            print("x win")
+
+        
     elif r%2 == 1:
         button_3.config(text="O")
         button_3.config(state=tk.DISABLED)
+
+        o_1x123 = o_1x123 + _1x3
+        o_123x3 = o_123x3 + _1x3
+        o_sec_diagonal = o_sec_diagonal + _1x3
+
+        if o_1x123 == 3:
+            print("o win")
+        elif o_123x3 == 3:
+            print("o win")
+        elif o_sec_diagonal == 3:
+            print("o win")
+                  
     r = r + 1
 
+'''
     _1x123 = _1x123 + _1x3
     _123x3 = _123x3 + _1x3
     _sec_diagonal = _sec_diagonal + _1x3
@@ -127,25 +203,47 @@ def button_3_c():
         print("win")
     elif _sec_diagonal == 3:
         print("win")
-
+'''
     
 
 def button_4_c():
     global r
     global _2x1
-    global _2x123
-    global _123x1
+    global o_2x123
+    global o_123x1
+    global x_2x123
+    global x_123x1
 
 
     if r%2 == 0:
         button_4.config(text="X")
         button_4.config(state=tk.DISABLED)
+
+        x_2x123 = x_2x123 + _2x1
+        x_123x1 = x_123x1 + _2x1
+
+        if x_2x123 == 3:
+            print("x win")
+        elif x_123x1 == 3:
+            print("x win")
+
+
     elif r%2 == 1:
         button_4.config(text="O")
         button_4.config(state=tk.DISABLED)
+
+        o_2x123 = o_2x123 + _2x1
+        o_123x1 = o_123x1 + _2x1
+
+        if o_2x123 == 3:
+            print("o win")
+        elif o_123x1 == 3:
+            print("o win")
+
+            
     r = r + 1
 
-
+'''
     _2x123 = _2x123 + _2x1
     _123x1 = _123x1 + _2x1
 
@@ -154,26 +252,64 @@ def button_4_c():
     elif _123x1 == 3:
         print("win")
 
-    
+'''    
 
 def button_5_c():
     global r
     global _2x2
-    global _2x123
-    global _123x2
-    global _pri_diagonal
-    global _sec_diagonal
+    global o_2x123
+    global o_123x2
+    global o_pri_diagonal
+    global o_sec_diagonal
+    global x_2x123
+    global x_123x2
+    global x_pri_diagonal
+    global x_sec_diagonal
 
 
     if r%2 == 0:
         button_5.config(text="X")
         button_5.config(state=tk.DISABLED)
+
+        x_2x123 = x_2x123 + _2x2
+        x_123x2 = x_123x2 + _2x2
+        x_pri_diagonal = x_pri_diagonal + _2x2
+        x_sec_diagonal = x_sec_diagonal + _2x2
+
+
+        if x_2x123 == 3:
+            print("x win")
+        elif x_123x2 == 3:
+            print("x win")
+        elif x_pri_diagonal == 3:
+            print("x win")
+        elif x_sec_diagonal == 3:
+            print("x win")
+
+        
     elif r%2 == 1:
         button_5.config(text="O")
         button_5.config(state=tk.DISABLED)
+
+        o_2x123 = o_2x123 + _2x2
+        o_123x2 = o_123x2 + _2x2
+        o_pri_diagonal = o_pri_diagonal + _2x2
+        o_sec_diagonal = o_sec_diagonal + _2x2
+
+
+        if o_2x123 == 3:
+            print("o win")
+        elif o_123x2 == 3:
+            print("o win")
+        elif o_pri_diagonal == 3:
+            print("o win")
+        elif o_sec_diagonal == 3:
+            print("o win")
+
+        
     r = r + 1
 
-
+'''
     _2x123 = _2x123 + _2x2
     _123x2 = _123x2 + _2x2
     _pri_diagonal = _pri_diagonal + _2x2
@@ -188,23 +324,46 @@ def button_5_c():
         print("win")
     elif _sec_diagonal == 3:
         print("win")
-    
+ '''
+
 
 def button_6_c():
     global r
     global _2x3
-    global _2x123
-    global _123x3
+    global o_2x123
+    global o_123x3
+    global x_2x123
+    global x_123x3
 
     if r%2 == 0:
         button_6.config(text="X")
         button_6.config(state=tk.DISABLED)
+
+        x_2x123 = x_2x123 + _2x3
+        x_123x3 = x_123x3 + _2x3
+
+        if x_2x123 == 3:
+            print("x win")
+        elif x_123x3 == 3:
+            print("x win")
+
+        
     elif r%2 == 1:
         button_6.config(text="O")
         button_6.config(state=tk.DISABLED)
+
+        o_2x123 = o_2x123 + _2x3
+        o_123x3 = o_123x3 + _2x3
+
+        if o_2x123 == 3:
+            print("o win")
+        elif o_123x3 == 3:
+            print("o win")
+
+            
     r = r + 1
 
-
+'''
     _2x123 = _2x123 + _2x3
     _123x3 = _123x3 + _2x3
 
@@ -212,25 +371,55 @@ def button_6_c():
         print("win")
     elif _123x3 == 3:
         print("win")
+'''
 
 
 def button_7_c():
     global r
     global _3x1
-    global _3x123
-    global _123x1
-    global _sec_diagonal
+    global o_3x123
+    global o_123x1
+    global o_sec_diagonal
+    global x_3x123
+    global x_123x1
+    global x_sec_diagonal
 
     
     if r%2 == 0:
         button_7.config(text="X")
         button_7.config(state=tk.DISABLED)
+
+        x_3x123 = x_3x123 + _3x1
+        x_123x1 = x_123x1 + _3x1
+        x_sec_diagonal = x_sec_diagonal + _3x1
+            
+        if x_3x123 == 3:
+            print("x win")
+        elif x_123x1 == 3:
+            print("x win")
+        elif x_sec_diagonal == 3:
+            print("x win")
+
+
     elif r%2 == 1:
         button_7.config(text="O")
         button_7.config(state=tk.DISABLED)
+
+        o_3x123 = o_3x123 + _3x1
+        o_123x1 = o_123x1 + _3x1
+        o_sec_diagonal = o_sec_diagonal + _3x1
+            
+        if o_3x123 == 3:
+            print("o win")
+        elif o_123x1 == 3:
+            print("o win")
+        elif o_sec_diagonal == 3:
+            print("o win")
+
+            
     r = r + 1
 
-
+'''
     _3x123 = _3x123 + _3x1
     _123x1 = _123x1 + _3x1
     _sec_diagonal = _sec_diagonal + _3x1
@@ -241,24 +430,48 @@ def button_7_c():
         print("win")
     elif _sec_diagonal == 3:
         print("win")
+'''
 
 
 def button_8_c():
     global r
     global _3x2
-    global _3x123
-    global _123x2
+    global o_3x123
+    global o_123x2
+    global x_3x123
+    global x_123x2
+
 
     
     if r%2 == 0:
         button_8.config(text="X")
         button_8.config(state=tk.DISABLED)
+
+        x_3x123 = x_3x123 + _3x2
+        x_123x2 = x_123x2 + _3x2
+
+        if x_3x123 == 3:
+            print("x win")
+        elif x_123x2 == 3:
+            print("x win")
+    
+        
     elif r%2 == 1:
         button_8.config(text="O")
         button_8.config(state=tk.DISABLED)
+
+        o_3x123 = o_3x123 + _3x2
+        o_123x2 = o_123x2 + _3x2
+
+        if o_3x123 == 3:
+            print("o win")
+        elif o_123x2 == 3:
+            print("o win")
+
+            
     r = r + 1
 
-
+'''
     _3x123 = _3x123 + _3x2
     _123x2 = _123x2 + _3x2
 
@@ -266,26 +479,55 @@ def button_8_c():
         print("win")
     elif _123x2 == 3:
         print("win")
-
+'''
 
 
 def button_9_c():
     global r
     global _3x3
-    global _3x123
-    global _123x3
-    global _pri_diagonal
+    global o_3x123
+    global o_123x3
+    global o_pri_diagonal
+    global x_3x123
+    global x_123x3
+    global x_pri_diagonal
 
     
     if r%2 == 0:
         button_9.config(text="X")
         button_9.config(state=tk.DISABLED)
+
+        x_3x123 = x_3x123 + _3x3
+        x_123x3 = x_123x3 + _3x3
+        x_pri_diagonal = x_pri_diagonal + _3x3
+
+        if x_3x123 == 3:
+            print("x win")
+        elif x_123x3 == 3:
+            print("x win")
+        elif x_pri_diagonal == 3:
+            print("x win")
+
+
     elif r%2 == 1:
         button_9.config(text="O")
         button_9.config(state=tk.DISABLED)
+
+        o_3x123 = o_3x123 + _3x3
+        o_123x3 = o_123x3 + _3x3
+        o_pri_diagonal = o_pri_diagonal + _3x3
+
+        if o_3x123 == 3:
+            print("o win")
+        elif o_123x3 == 3:
+            print("o win")
+        elif o_pri_diagonal == 3:
+            print("o win")
+
+
     r = r + 1
      
-    
+'''    
     _3x123 = _3x123 + _3x3
     _123x3 = _123x3 + _3x3
     _pri_diagonal = _pri_diagonal + _3x3
@@ -297,7 +539,7 @@ def button_9_c():
     elif _pri_diagonal == 3:
         print("win")
 
-
+'''
 
 pixel = tk.PhotoImage(width=1, height=1)
 
