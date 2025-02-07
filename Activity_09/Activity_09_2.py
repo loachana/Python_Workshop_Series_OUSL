ff6ef4a2456d1294e5f09d2d94988c3bcf784b69
unknown <lochanaeg@gmail.com>
2025-02-07 17:01:54 +0530

    Act_9_update

---
+++ Activity_09/Activity_09_2.py
@@ -0,0 +1,11 @@
+#2/2/2025
+#This is the answer for anctivity 9
+#All rights reserved
+
+the_file = open("my_notes.txt", "a")
+
+the_file.write("\n"+"I am learning how to handle files!")
+
+the_file.close()
+
+
---
+++ Activity_09/Activity_09_3.py
@@ -0,0 +1,13 @@
+#2/2/2025
+#This is the answer for anctivity 9
+#All rights reserved
+
+the_file = open("my_notes.txt", "r")
+
+text = the_file.read()
+
+print(text)
+
+the_file.close()
+
+
